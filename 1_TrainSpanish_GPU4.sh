0894211c5025fac9a4c49315fcbaab73d31ba3fb
ivangtorre <ivangonzaleztorre@gmail.com>
2021-11-26 12:29:12 +0100

    Init

--- 1_TrainSpanish_GPU4.sh
+++ 1_TrainSpanish_GPU4.sh
@@ -32,8 +32,6 @@ nvidia-docker run -it -d --rm --network=host --name ${CONTAINER} --runtime=nvidi
 -v /DATA/GRACE/IVANbackup/GRACE_train/DATA/:/DATA/ \
 -v /DATA/GRACE/IVANbackup/GRACE_train/datasets/:/datasets/ \
 -v /DATA/GRACE/IVANbackup/GRACE_train/datasets/ALBAYZIN2020/:/DATA/TMP_IVAN/jasper/datasets/ALBAYZIN2020/ \
-
-
 -v /home:/home \
 -v $PWD:/workspace/wav2vec2 \
 -w $PWD ${NAME}
