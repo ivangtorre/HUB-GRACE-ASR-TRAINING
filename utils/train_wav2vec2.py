3d8297da072a0c8f65e21626779fbba2d61cb75f
ivangtorre <ivangonzaleztorre@gmail.com>
2021-11-30 12:24:06 +0100

    Init

--- utils/train_wav2vec2.py
+++ utils/train_wav2vec2.py
@@ -432,6 +432,7 @@ def main():
         train_dataset=train_dataset if training_args.do_train else None,
         eval_dataset=eval_dataset if training_args.do_eval else None,
         tokenizer=processor.feature_extractor,
+        use_apex=True
     )
 
     # START TRAINING ##---------------------------------------------
