3e8655372f0e1f8f789bb1bdcb987a24b42061b5
ivangtorre <ivangonzaleztorre@gmail.com>
2021-11-30 09:59:36 +0100

    Init

--- 1_TrainSpanish_GPU4_example.sh
+++ 1_TrainSpanish_GPU4_example.sh
@@ -43,22 +43,20 @@ set +x
 # Execute
 nvidia-docker exec -it ${CONTAINER} python -m torch.distributed.launch \
 	--nproc_per_node 2 utils/example8gpu.py \
-	--dataset_name="librispeech_asr" \
-	--model_name_or_path="facebook/wav2vec2-large-lv60" \
-	--dataset_config_name="clean" \
-	--train_split_name="train.100" \
-	--eval_split_name="validation" \
-	--output_dir="./wav2vec2-librispeech-clean-100h-demo-dist" \
+  --dataset_name="common_voice" \
+	--model_name_or_path="facebook/wav2vec2-large-xlsr-53" \
+	--dataset_config_name="tr" \
+	--output_dir="./wav2vec2-common_voice-tr-demo-dist" \
 	--preprocessing_num_workers="16" \
 	--overwrite_output_dir \
-	--num_train_epochs="3" \
+	--num_train_epochs="15" \
 	--per_device_train_batch_size="4" \
 	--gradient_accumulation_steps="1" \
 	--learning_rate="3e-4" \
 	--warmup_steps="500" \
 	--evaluation_strategy="steps" \
-	--audio_column_name="file" \
-	--text_column_name="text" \
+	--audio_column_name="path" \
+	--text_column_name="sentence" \
 	--save_steps="400" \
 	--eval_steps="100" \
 	--logging_steps="1" \
@@ -66,7 +64,7 @@ nvidia-docker exec -it ${CONTAINER} python -m torch.distributed.launch \
 	--save_total_limit="3" \
 	--freeze_feature_extractor \
 	--gradient_checkpointing \
-	--chars_to_ignore , ? . ! - \; \: \" “ % ‘ ” \
+	--chars_to_ignore , ? . ! - \; \: \" “ % ‘ ” � \
 	--fp16 \
 	--group_by_length \
 	--push_to_hub \
