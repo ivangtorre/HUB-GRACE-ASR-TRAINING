117c54085c355396192d11962d7cbbe0235b88b1
ivangtorre <ivangonzaleztorre@gmail.com>
2021-12-02 09:53:53 +0100

    Init

--- 3_EvaluationSpanish_GPU4.sh
+++ 3_EvaluationSpanish_GPU4.sh
@@ -27,7 +27,14 @@ fi
 
 # Run the container
 set -x
-nvidia-docker run --network=host -it -d --rm --name ${CONTAINER} --runtime=nvidia --shm-size=4g --ulimit memlock=-1 --ulimit stack=67108864 -v /DATA/GRACE/IVANbackup/node2/DATA/TMP_IVAN:/DATA/TMP_IVAN  -v /DATA/GRACE/IVANbackup/node2/DATA/:/DATA/:ro -w $PWD -v /home:/home -v $PWD:/workspace/wav2vec2/ ${NAME}
+nvidia-docker run -it -d --rm --ipc=host --network=host --name ${CONTAINER} --runtime=nvidia --shm-size=16g --ulimit memlock=-1 --ulimit stack=67108864 \
+-v C:/DATA/TMP_IVAN \
+-v /DATA/GRACE/IVANbackup/GRACE_train/DATA/:/DATA/ \
+-v /DATA/GRACE/IVANbackup/GRACE_train/datasets/:/datasets/ \
+-v /DATA/GRACE/IVANbackup/GRACE_train/datasets/ALBAYZIN2020/trainspeedall/:/DATA/TMP_IVAN/jasper/datasets/ALBAYZIN2020/trainspeedall/ \
+-v /home:/home \
+-v $PWD:/workspace/wav2vec2 \
+-w $PWD ${NAME}
 set +x
 
 # Execute
