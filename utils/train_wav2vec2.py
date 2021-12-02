492fd8f2223c91d49a9dc26bae96424f301b98ce
ivangtorre <ivangonzaleztorre@gmail.com>
2021-12-02 10:24:43 +0100

    Init

--- utils/train_wav2vec2.py
+++ utils/train_wav2vec2.py
@@ -334,7 +334,9 @@ def main():
 
     # Load pretrained model and tokenizer
     tokenizer = Wav2Vec2CTCTokenizer(training_args.output_dir + "/vocab.json", unk_token="[UNK]", pad_token="[PAD]", word_delimiter_token="|")
-    feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, sampling_rate=16_000, padding_value=0.0, do_normalize=True, return_attention_mask=True)
+
+    feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_args.model_name_or_path)
+    #feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, sampling_rate=16_000, padding_value=0.0, do_normalize=True, return_attention_mask=True)
     processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
     model = Wav2Vec2ForCTC.from_pretrained(
         model_args.model_name_or_path,
