fe7d2cb82fc8dd13bf7dee57c259e226ee56e7f3
ivangtorre <ivangonzaleztorre@gmail.com>
2021-12-09 18:30:21 +0100

    Init

---
+++ 4_TranscribeChallenge.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#####################################
+# Script to initialize the server
+#####################################
+
+# VARIABLES ####################
+NAME=${NAME:-"wav2vec2"}
+CONTAINER=${CONTAINER:-"wav2vec2_transcribe"}
+export NV_GPU="0"
+
+################################
+################################
+
+# Ensure that the server is closed when the script exits
+function cleanup_server {
+    echo "Shutting down ${CONTAINER} container"
+    docker stop ${CONTAINER} > /dev/null 2>&1
+}
+
+if [ "$(docker inspect -f "{{.State.Running}}" ${CONTAINER})" = "true" ]; then
+    if [ "$1" == "norestart" ]; then
+       echo "${CONTAINER} is already running ..."
+       exit 0
+    fi
+    cleanup_server || true
+fi
+
+
+# Run the container
+set -x
+nvidia-docker run -it -d --rm --ipc=host --network=host --name ${CONTAINER} --runtime=nvidia --shm-size=16g --ulimit memlock=-1 --ulimit stack=67108864 \
+-v /DATA:/DATA/ \
+-v /DATA/GRACE/IVANbackup/GRACE_train/DATA/:/DATA/ \
+-v /DATA/GRACE/IVANbackup/GRACE_train/datasets/:/datasets/ \
+-v /home:/home \
+-v $PWD:/workspace/wav2vec2 \
+-w $PWD ${NAME}
+set +x
+
+# Execute
+nvidia-docker exec -it wav2vec2_0 python3 utils/eval_transcript.py \
+--model_path=/DATA/GRACE/IVAN_VICOM/MODELS_W2V2/SPANISH/epoch_5_lr_15/spanish.-EPOCH-5.-21-12-05.18.23.54/ \
+--test_paths=test_challenge.csv
+--lm_path=/DATA/TRANSCRIPTIONS+ALACARTA+RTVE24H2017.5gram.arpa \
+--alpha=0.4 \
+--beta=2 \
+--beam_width=256 \
+--test_paths=test_challenge.csv
+
+
+END=$(date +%s.%N)
+echo "TOTAL TIME to transcribe....(seconds).............."
+DIFF=$(echo "$END - $START" | bc)
+echo $DIFF
+echo "...................................................."
