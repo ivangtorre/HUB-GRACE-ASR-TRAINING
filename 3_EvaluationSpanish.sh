0dd3d5ae6d4cc5ecf3f563571dad58647b3cf355
ivangtorre <ivangonzaleztorre@gmail.com>
2021-12-14 10:30:23 +0100

    Init

--- 3_EvaluationSpanish.sh
+++ 3_EvaluationSpanish.sh
@@ -29,7 +29,7 @@ fi
 set -x
 nvidia-docker run -it -d --rm --ipc=host --network=host --name ${CONTAINER} --runtime=nvidia --shm-size=16g --ulimit memlock=-1 --ulimit stack=67108864 \
 #-v /DATA/GRACE/IVANbackup/GRACE_train/DATA/:/DATA/ \
--v /DATA/GRACE/IVANbackup/node2/DATA/:/DATA/
+-v /DATA/GRACE/IVANbackup/node2/DATA/:/DATA/ \
 -v /DATA/GRACE/IVANbackup/GRACE_train/datasets/:/datasets/ \
 -v /home:/home \
 -v $PWD:/workspace/wav2vec2 \
