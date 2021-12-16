601b4fdc5ce3481b46ed3278b5991dff1adc9150
ivangtorre <ivangonzaleztorre@gmail.com>
2021-12-16 08:42:17 +0100

    Init

--- Experiments/SpanishEvaluation.sh
+++ Experiments/SpanishEvaluation.sh
@@ -2,8 +2,8 @@
 
 ##### PARAMETERS ##################################
 #MODEL_PATH=${1:-${MODEL_PATH:-"results/xls-r-300m/spanish.-EPOCH-5.-21-12-05.18.23.54"}}
-MODEL_PATH=${1:-${MODEL_PATH:-"results/xls-r-300m/spanish.-EPOCH-15.-21-12-07.20.51.59/checkpoint-27500"}}
-#MODEL_PATH=${1:-${MODEL_PATH:-"checkpoint-30500"}}
+#MODEL_PATH=${1:-${MODEL_PATH:-"results/xls-r-300m/spanish.-EPOCH-15.-21-12-07.20.51.59/checkpoint-27500"}}
+MODEL_PATH=${1:-${MODEL_PATH:-"checkpoint-34000"}}
 #MODEL_PATH=${1:-${MODEL_PATH:-"models/spanish.-EPOCH-20.-21-09-15.22.11.49/"}}
 DATA_DIR=${2:-${DATA_DIR:-""}}
 TEST_1=${3:-${TEST_1:-"/datasets/spanish/tests/test_ES_MEDIA.csv"}}
