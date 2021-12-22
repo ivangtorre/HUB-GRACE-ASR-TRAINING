be2442a307c92075b6c1a65ba240434961504409
ivangtorre <ivangonzaleztorre@gmail.com>
2021-12-22 12:12:52 +0100

    Init

---
+++ 2_TrainEnglish_GPU0.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#####################################
+# Script to initialize the server
+#####################################
+
+# VARIABLES ####################
+NAME=${NAME:-"wav2vec2"}
+CONTAINER=${CONTAINER:-"wav2vec2train_0"}
+export NV_GPU="0"
+
+################################
+################################
+
+# Ensure that the server is closed when the script exits
+function cleanup_server {
+    echo "Shutting down ${CONTAINER} container"
+    docker stop ${CONTAINER} > /dev/null 2>&1
+}
+
+if [ "$(docker inspect -f "{{.State.Running}}" ${CONTAINER})" = "true" ]; then
+    if [ "$1" == "norestart" ]; then
+       echo "${CONTAINER} is already running ..."
+       exit 0
+    fi
+    cleanup_server || true
+fi
+
+# Run the container
+set -x
+nvidia-docker run -it -d --rm --ipc=host --network=host --name ${CONTAINER} --runtime=nvidia --shm-size=16g --ulimit memlock=-1 --ulimit stack=67108864 \
+-v /DATA/:/DATA/ \
+-v /DATA/GRACE/IVAN/datasets/:/datasets/:ro \
+#-v /DATA/GRACE/IVAN/datasets/ALBAYZIN2020/trainspeedall/:/DATA/TMP_IVAN/jasper/datasets/ALBAYZIN2020/trainspeedall/:ro \
+-v /home:/home \
+-v $PWD:/workspace/wav2vec2 \
+-w $PWD ${NAME}
+set +x
+
+#-v /DATA/GRACE/IVANbackup/GRACE_train/DATA/:/DATA/ \
+#-v /DATA/GRACE/IVANbackup/GRACE_train/datasets/:/datasets/ \
+#-v /DATA/GRACE/IVANbackup/GRACE_train/datasets/ALBAYZIN2020/trainspeedall/:/DATA/TMP_IVAN/jasper/datasets/ALBAYZIN2020/trainspeedall/ \
+#-v /home:/home \
+#-v $PWD:/workspace/wav2vec2 \
+#-w $PWD ${NAME}
+
+
+
+
+
+#nvidia-docker exec -it JasperTRT3 bash scripts/CHALLENGE_DECODING/Experiments/inference2logits.sh
+# Execute
+nvidia-docker exec -it ${CONTAINER} bash Experiments/EnglishTrain_GPU0.sh
