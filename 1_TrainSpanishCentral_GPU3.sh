0e1901c600804281bda5a5250a762fda055955ef
ivangtorre <ivangonzaleztorre@gmail.com>
2022-01-07 10:17:46 +0100

    Init

--- 1_TrainSpanishCentral_GPU3.sh
+++ 1_TrainSpanishCentral_GPU3.sh
@@ -29,7 +29,7 @@ fi
 set -x
 nvidia-docker run -it -d --rm --ipc=host --network=host --name ${CONTAINER} --runtime=nvidia --shm-size=16g --ulimit memlock=-1 --ulimit stack=67108864 \
 -v /DATA/GRACE/IVAN_VICOM/DATA/:/DATA/ \
--v /DATA/GRACE/IVAN_VICOM/:/MODELS/:ro \
+-v /DATA/GRACE/IVAN_VICOM/MODELS/:/MODELS/:ro \
 -v /DATA/GRACE/IVAN_VICOM/w2v2/:/w2v2/ \
 -v /DATA/GRACE/IVAN_VICOM/datasets/:/datasets/ \
 -v /DATA/GRACE/IVAN_VICOM/datasets/ALBAYZIN2020/trainspeedall/:/DATA/TMP_IVAN/jasper/datasets/ALBAYZIN2020/trainspeedall/ \
