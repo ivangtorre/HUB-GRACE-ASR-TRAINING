5859371c37f6abdeb52df1dd524fe098a08f35b3
ivangtorre <ivangonzaleztorre@gmail.com>
2022-01-13 17:24:51 +0100

    Init

--- Finetune_Example/train_w2v2_example.py
+++ Finetune_Example/train_w2v2_example.py
@@ -331,9 +331,9 @@ def main():
         return {"vocab": [vocab], "all_text": [all_text]}
 
     vocab_train = train_dataset.map(extract_all_chars, batched=True, batch_size=-1, keep_in_memory=True,
-                                         remove_columns=train_dataset.column_names)
+                                    remove_columns=train_dataset.column_names)
     vocab_test = eval_dataset.map(extract_all_chars, batched=True, batch_size=-1, keep_in_memory=True,
-                                       remove_columns=eval_dataset.column_names)
+                                  remove_columns=eval_dataset.column_names)
 
     vocab_list = list(set(vocab_train["vocab"][0]) | set(vocab_test["vocab"][0]))
     vocab_dict = {v: k for k, v in enumerate(vocab_list)}
