810a722a8badb9fd3656e594e21032cc6edab24e
Ivan <ivangonzaleztorre@gmail.com>
2022-01-26 22:29:23 +0100

    commit

--- utils/transcribe.py
+++ utils/transcribe.py
@@ -46,35 +46,35 @@ def decode2logits(data_file, processor, model):
 
 
 def transcribe(line, decoder, args):
-    print(len(line))
-
-    ###INCORPORATE SPLIT
-    size2split = 50000
-    if len(line) < size2split:
-        beams = decoder.decode_beams(line, args.beam_width)
-        lista_nbeams = [item[0] for item in beams][0]
-
-    else:
-        total_size =len(line)
-        current_size = 0
-        lista_nbeams = ""
-        while current_size<total_size:
-            beams = decoder.decode_beams(line[current_size:current_size+size2split], args.beam_width)
-            temp_list = [item[0] for item in beams][0]
-            lista_nbeams = lista_nbeams + temp_list
-            current_size += size2split
-
-    # WITH NO SPLIT
-    # if len(line) < 150000:
-    #     beams = decoder.decode_beams(line, args.beam_width)
+    # print(len(line))
     #
-    # elif 150000 <= len(line) < 250000:
-    #     beams = decoder.decode_beams(line, args.beam_width, prune_history=False, beam_prune_logp=-7, token_min_logp=-5)
+    # ###INCORPORATE SPLIT
+    # size2split = 50000
+    # if len(line) < size2split:
+    #     beams = decoder.decode_beams(line, args.beam_width)
+    #     lista_nbeams = [item[0] for item in beams][0]
     #
     # else:
-    #     beams = decoder.decode_beams(line, 128)
-    #
-    # lista_nbeams = [item[0] for item in beams][0]
+    #     total_size =len(line)
+    #     current_size = 0
+    #     lista_nbeams = ""
+    #     while current_size<total_size:
+    #         beams = decoder.decode_beams(line[current_size:current_size+size2split], args.beam_width)
+    #         temp_list = [item[0] for item in beams][0]
+    #         lista_nbeams = lista_nbeams + temp_list
+    #         current_size += size2split
+
+    # WITH NO SPLIT
+    if len(line) < 150000:
+        beams = decoder.decode_beams(line, args.beam_width)
+
+    elif 150000 <= len(line) < 250000:
+        beams = decoder.decode_beams(line, args.beam_width, prune_history=False, beam_prune_logp=-7, token_min_logp=-5)
+
+    else:
+        beams = decoder.decode_beams(line, 128)
+
+    lista_nbeams = [item[0] for item in beams][0]
 
 
     if args.save:
@@ -92,15 +92,15 @@ def transcribe(line, decoder, args):
     end = []
     wd = []
     ones = []
-    #for item in indices:
-    #    wd.append(item[0])
-    #    begin.append(item[1][0] * w)
-    #    end.append(item[1][1] * w)
-    #    ones.append(1)
+    for item in indices:
+       wd.append(item[0])
+       begin.append(item[1][0] * w)
+       end.append(item[1][1] * w)
+       ones.append(1)
     # print(lista_nbeams)
-    for i, item in enumerate(lista_nbeams.split(" ")):
-        wd.append(item)
-        begin.append(i)
-        end.append(i)
-        ones.append(1)
+    # for i, item in enumerate(lista_nbeams.split(" ")):
+    #     wd.append(item)
+    #     begin.append(i)
+    #     end.append(i)
+    #     ones.append(1)
     return (pd.DataFrame({"start": begin, "end": end, "conf": ones, "words": wd}))
