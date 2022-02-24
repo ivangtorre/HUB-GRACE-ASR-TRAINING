c16a3e1bce854e0dbaf5117b6fed3a525cc6c227
ivangtorre <ivangonzaleztorre@gmail.com>
2022-02-24 10:26:44 +0100

    Init

---
+++ 5_1_TrainGerman.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#####################################
+# Script to initialize the server
+#####################################
+
+# VARIABLES ####################
+NAME=${NAME:-"wav2vec2"}
+CONTAINER=${CONTAINER:-"wav2vec2t2"}
+export NV_GPU="1"
+
+################################
+################################
+
+# Ensure that the server is closed when the script exits
+function cleanup_server {
+    echo "Shutting down ${CONTAINER} container"
+    docker stop ${CONTAINER} > /dev/null 2>&1
+}
+
+if [ "$(docker inspect -f "{{.State.Running}}" ${CONTAINER})" = "true" ]; then
+    if [ "$1" == "norestart" ]; then
+       echo "${CONTAINER} is already running ..."
+       exit 0
+    fi
+    cleanup_server || true
+fi
+
+# Run the container
+set -x
+nvidia-docker run -it -d --rm --ipc=host --network=host --name ${CONTAINER} --runtime=nvidia --shm-size=16g --ulimit memlock=-1 --ulimit stack=67108864 \
+-v /DATA/GRACE/IVAN_VICOM/DATA:/DATA/ \
+-v /home:/home \
+-v $PWD:/workspace/wav2vec2 \
+-w $PWD ${NAME}
+set +x
+
+
+#nvidia-docker exec -it JasperTRT3 bash scripts/CHALLENGE_DECODING/Experiments/inference2logits.sh
+# Execute
+nvidia-docker exec -it ${CONTAINER} bash Experiments/GermanTRAIN.sh
