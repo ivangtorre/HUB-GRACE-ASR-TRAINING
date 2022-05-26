3ac901fee1a6560ee120286d8926d2977150008b
ivangtorre <ivangonzaleztorre@gmail.com>
2022-05-26 10:36:50 +0200

    Init

--- APIinference/federatedlearning/custom/get_data.py
+++ APIinference/federatedlearning/custom/get_data.py
@@ -12,7 +12,7 @@ from transformers import (
 )
 
 def train_test_dataset():
-    tokenizer = Wav2Vec2CTCTokenizer("vocab.json", unk_token="[UNK]", pad_token="[PAD]", word_delimiter_token="|")
+    tokenizer = Wav2Vec2CTCTokenizer("/home/VICOMTECH/igonzalez/vocab.json", unk_token="[UNK]", pad_token="[PAD]", word_delimiter_token="|")
     feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, sampling_rate=16_000, padding_value=0.0,
                                                  do_normalize=True, return_attention_mask=True)
     processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
