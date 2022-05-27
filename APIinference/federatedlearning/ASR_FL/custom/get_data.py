b9adb263db94ddf44eb2d947293b6dfdcabcedc4
ivangtorre <ivangonzaleztorre@gmail.com>
2022-05-27 13:41:46 +0200

    Init

--- APIinference/federatedlearning/ASR_FL/custom/get_data.py
+++ APIinference/federatedlearning/ASR_FL/custom/get_data.py
@@ -1,3 +1,5 @@
+import os
+
 import datasets
 from datasets import Audio
 from transformers import (
@@ -7,6 +9,7 @@ from transformers import (
 )
 
 def train_test_dataset():
+    print(os.getcwd(), flush=True)
     tokenizer = Wav2Vec2CTCTokenizer("./vocab.json", unk_token="[UNK]", pad_token="[PAD]", word_delimiter_token="|")
     feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, sampling_rate=16_000, padding_value=0.0,
                                                  do_normalize=True, return_attention_mask=True)
