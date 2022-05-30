35c047cedce795bb227d33244b8f8750a0e7543c
ivangtorre <ivangonzaleztorre@gmail.com>
2022-05-30 16:50:17 +0200

    Init

--- APIinference/federatedlearning/custom/pt_constants.py
+++ APIinference/federatedlearning/custom/pt_constants.py
@@ -2,11 +2,12 @@
 Copied from https://github.com/NVIDIA/NVFlare/blob/main/examples/hello-pt/custom/pt_constants.py
 Still not sure the exact role of these variables (apart from the evident), so for now I leave them exactly as they came
 """
-import os
+
+
 class PTConstants:
     PTServerName = "server"
     PTFileModelName = "FL_global_model.pt"
     PTLocalModelName = "local_model.pt"
+
     PTModelsDir = "models"
     CrossValResultsJsonFilename = "cross_val_results.json"
-    vocabfile = os.path.dirname(__file__) + "vocab.txt"
